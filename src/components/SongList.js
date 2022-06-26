a9b29b0f2a2fdd4d6b0ab5d6b6ff5c77a4760fb3
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-06-26 14:52:51 +0200

    basic styling with tailwind

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -5,14 +5,15 @@ class SongList extends Component {
   renderList() {
     return this.props.songs.map((song) => {
       return (
-        <div className="flex" key={song.title}>
-          <div className="flex-none">
+        <div className="flex justify-between" key={song.title}>
+          <div className="flex items-center p-2">
             <p>{song.title}</p>
+            <p className="px-4">({song.duration})</p>
           </div>
-          <div className="flex-none">
+          <div className="flex p-2">
             <button
               className="inline-block px-4 py-2 bg-white border border-indigo-700 text-indigo-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700
-             hover:shadow-lg hover:text-white transition duration-150 ease-in-out"
+             hover:shadow-sm hover:shadow-black hover:text-white transition duration-150 ease-in-out"
             >
               Select
             </button>
@@ -24,7 +25,9 @@ class SongList extends Component {
 
   render() {
     return (
-      <div className="flex flex-col h-full bg-white">{this.renderList()}</div>
+      <div className="flex flex-col p-8 bg-white rounded shadow-sm shadow-black">
+        {this.renderList()}
+      </div>
     );
   }
 }
